6a4e59e1c87d450fce14d432003c092391f11aee
JohnpaulOlingay-UCM <johnpaulolingay38@gmail.com>
2025-08-29 08:04:42 +0800

    FavoriteSong.java

---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,8 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("Line 1 of my favorite song...");
+        System.out.println("Line 2 of my favorite song...");
+        System.out.println("Line 3 of my favorite song...");
+        System.out.println("Line 4 of my favorite song...");
+    }
+}
