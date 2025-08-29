abca8c62654690580e3b4a9f539900b7e1f86ea7
JohnpaulOlingay-UCM <johnpaulolingay38@gmail.com>
2025-08-29 08:23:00 +0800

    Comments.java

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,12 @@
+public class Comments  {
+
+@param args command-line arguments (not used)
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("This program demonstrates the three types of Java comments.");
+        System.out.println(" - Javadoc comments (/** ... */)");
+        System.out.println(" - Multi-line comments (/* ... */)");
+        System.out.println(" - Single-line comments (// ...)");
+    }
+}
